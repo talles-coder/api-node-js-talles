e08e620739179632e802d115b11dc718f626b7ac
talles_coder <talleswendrel@gmail.com>
2021-08-17 18:43:16 -0300

    mod port from process.env

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use('/api/v1/tasks', tasks)
 const start = async () => {
     try {
         await connectDB(process.env.MONGO_URI)
-        app.listen(process.env.PORT || 3000)
+        app.listen(process.env.PORT || 8888)
     } catch (error) {
         console.log(error)
     }
