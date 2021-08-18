892bfdc87a8065930e01138abdc8a39aaf5d5831
talles_coder <talleswendrel@gmail.com>
2021-08-18 11:56:28 -0300

    completed the creation of task manager web site/api

--- app.js
+++ app.js
@@ -4,18 +4,11 @@ const tasks = require('./routes/tasks')
 const connectDB = require('./db/connect')
 require('dotenv').config()
 //middleware
-
+app.use(express.static('./public'))
 app.use(express.json())
 
 
 // routes
-app.get('/', (req ,res) => {
-    res.send('Ok')
-})
-
-app.get('/hello', (req ,res) => {
-    res.send('Task Manager App')
-})
 
 app.use('/api/v1/tasks', tasks)
 
